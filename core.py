b38f2287dda7c8484828c1a21ff770a5040559be
Ravil Galeev <pink.ravil02@gmail.com>
2022-10-24 17:34:00 +0200

    first commit

---
+++ core.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe as mp
+import time
+cap = cv2.VideoCapture(0)
+
+mpHand = mp.solutions.hands
+hands = mpHand.Hands(static_image_mode=False,
+                     max_num_hands=2,
+                     min_detection_confidence=0.5,
+                     min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, frame = cap.read()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(image)
+
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.circle(frame, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(frame, handLms, mpHand.HAND_CONNECTIONS)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+
+
